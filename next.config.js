bac20ad38ab962ed85e25749fcebd19ee9728b66
amandalimon <amandalimon@outlook.com>
2024-07-26 11:42:54 -0600

    homepage

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
 const config = {
   future: {
     webpack5: true,
   },
-}
+  env: {
+    NEXT_PUBLIC_SPACE_ID: process.env.NEXT_PUBLIC_SPACE_ID,
+    NEXT_PUBLIC_ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+  },
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
+};
 
-module.exports = withBundleAnalyzer(config)
+module.exports = withBundleAnalyzer(config);
